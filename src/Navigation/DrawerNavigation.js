c1b55f9ff9ce0f15b157f30fec74277685ee2676
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-11-24 16:48:04 +0530

    added navigation constants

--- src/Navigation/DrawerNavigation.js
+++ src/Navigation/DrawerNavigation.js
@@ -1,13 +1,23 @@
 import React, { memo } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import {
+  DASHBOARD_SCREEN,
+  TRANSACTIONS_SCREEN,
+  EXCHANGE_SCREEN,
+} from './NavConstants';
+import DashBoard from '../Screens/DashBoard';
+import Transactions from '../Screens/Transactions';
+import Exchange from '../Screens/Exchange';
+
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigation() {
   return (
     <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      <Drawer.Screen name={DASHBOARD_SCREEN} component={DashBoard} />
+      <Drawer.Screen name={TRANSACTIONS_SCREEN} component={Transactions} />
+      <Drawer.Screen name={EXCHANGE_SCREEN} component={Exchange} />
     </Drawer.Navigator>
   );
 }
