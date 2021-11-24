0c618853b9b291c7ced7cf0e7894aaa2ebe12c21
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-11-24 16:43:50 +0530

    added drawer screen components

---
+++ src/Screens/DashBoard.js
@@ -0,0 +1,14 @@
+import React, { memo } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const DashBoard = () => {
+  return (
+    <View>
+      <Text>DashBoard</Text>
+    </View>
+  );
+};
+
+export default memo(DashBoard);
+
+const styles = StyleSheet.create({});
---
+++ src/Screens/Transactions.js
@@ -0,0 +1,14 @@
+import React, { memo } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Transactions = () => {
+  return (
+    <View>
+      <Text>Transactions</Text>
+    </View>
+  );
+};
+
+export default memo(Transactions);
+
+const styles = StyleSheet.create({});
