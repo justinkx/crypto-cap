0995ccacd64fd5eaaec15c5dd88e388aae0e0825
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-11-29 10:27:39 +0530

    added custom fonts

---
+++ src/utils/Typography.js
@@ -0,0 +1,8 @@
+export const customFonts = {
+  montserratRegular: require('../../assets/fonts/Montserrat-Regular.ttf'),
+  montserratMedium: require('../../assets/fonts/Montserrat-Medium.ttf'),
+  montserratSemiBold: require('../../assets/fonts/Montserrat-SemiBold.ttf'),
+  montserratBold: require('../../assets/fonts/Montserrat-Bold.ttf'),
+  outfitRegular: require('../../assets/fonts/Outfit-Regular.ttf'),
+  outfitBold: require('../../assets/fonts/Outfit-Bold.ttf'),
+};
