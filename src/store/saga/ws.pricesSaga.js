04d45c9fd70e7fcebddcc7737c49690940e7d258
justinkx <justinkizhakkeparampil@gmail.com>
2021-12-31 00:24:26 +0530

    integrated price change socket connection

--- src/store/saga/ws.pricesSaga.js
+++ src/store/saga/ws.pricesSaga.js
@@ -2,10 +2,11 @@ import { put, take } from 'redux-saga/effects';
 import { connect } from '@giantmachines/redux-websocket';
 
 import { CONNECT_SOCKET } from '../actions/socketAction';
+import { PRICES_SOCKET } from '../../utils/api';
 
 export default function* websocketConnectionSaga() {
   while (true) {
     yield take(CONNECT_SOCKET);
-    yield put(connect('wss://stream.binance.com:9443/ws'));
+    yield put(connect(PRICES_SOCKET));
   }
 }
