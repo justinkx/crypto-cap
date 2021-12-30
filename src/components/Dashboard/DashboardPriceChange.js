3eb9b0ee2fbf4096af4d678f7dcc92190413b5be
justinkx <justinkizhakkeparampil@gmail.com>
2021-12-30 21:30:20 +0530

    added price change 24hr

--- src/components/Dashboard/DashboardPriceChange.js
+++ src/components/Dashboard/DashboardPriceChange.js
@@ -1,10 +1,18 @@
 import React, { memo, useCallback } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Image, View, FlatList, Text } from 'react-native';
 import { useSelector, shallowEqual } from 'react-redux';
 
-import { commonStyles, colors } from '../../styles/CommonStyles';
+import {
+  commonStyles,
+  colors,
+  FONT_BOLD,
+  FONT_MEDIUM,
+} from '../../styles/CommonStyles';
 import { getCryptoAssets } from '../../store/selectors/assetSelector';
 import { balanceCoins } from '../../utils/data';
+import { CRYPTO_ASSET_SMALL } from '../../utils/api';
+
+const HEIGHT = 75;
 
 const DashboardPriceChange = () => {
   const balanceMarketChanges = useSelector(
@@ -31,19 +39,18 @@ const DashboardPriceChange = () => {
 
 const RenderBalance = memo(
   ({
-    baseId,
-    baseSymbol,
-    exchangeId,
-    percentExchangeVolume,
-    priceQuote,
+    changePercent24Hr,
+    explorer,
+    id,
+    marketCapUsd,
+    name,
     priceUsd,
-    quoteId,
-    quoteSymbol,
-    rank,
-    tradesCount24Hr,
-    updated,
     volumeUsd24Hr,
+    symbol,
   }) => {
+    const change24Hr = parseFloat(changePercent24Hr).toFixed(2);
+    const isUp = change24Hr > 0;
+
     return (
       <View
         style={[
@@ -52,7 +59,32 @@ const RenderBalance = memo(
           styles.cardContainer,
         ]}
       >
-        <View style={[styles.boxShadow, styles.tickerCard]}></View>
+        <View
+          style={[
+            styles.boxShadow,
+            styles.tickerCard,
+            commonStyles.row,
+            commonStyles.spaceBetween,
+          ]}
+        >
+          <View style={[styles.leftView, commonStyles.row]}>
+            <Image
+              style={styles.icon}
+              source={{ uri: CRYPTO_ASSET_SMALL(symbol) }}
+            />
+            <View>
+              <Text style={styles.name}>{name}</Text>
+              <Text style={styles.priceUsd}>
+                ${parseFloat(priceUsd).toFixed(2)}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.rightView}>
+            <Text style={[styles.change24, isUp ? styles.up : styles.down]}>
+              {change24Hr}% {isUp ? '▲' : '▼'}
+            </Text>
+          </View>
+        </View>
         <View style={[styles.boxShadow, styles.tickerBuy]}></View>
       </View>
     );
@@ -67,15 +99,23 @@ const styles = StyleSheet.create({
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 1,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
     borderRadius: 10,
-    height: 70,
+    height: HEIGHT,
     backgroundColor: colors.white,
   },
-  tickerCard: { width: '80%' },
-  tickerBuy: { width: '15%' },
+  tickerCard: { flex: 1, padding: 5, alignItems: 'center' },
+  leftView: {},
+  rightView: {},
+  tickerBuy: { width: HEIGHT, height: HEIGHT, marginLeft: 15 },
+  icon: { width: 35, height: 35, marginRight: 5 },
+  name: { fontFamily: FONT_MEDIUM, fontSize: 13 },
+  priceUsd: { fontFamily: FONT_BOLD, fontSize: 14, paddingTop: 2 },
+  change24: { fontSize: 12, fontFamily: FONT_MEDIUM },
+  up: { color: colors.success },
+  down: { color: colors.error },
 });
