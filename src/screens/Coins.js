b7c7dc8698374a747ee05dc1eae1def768ba09ea
justinkx <justinkizhakkeparampil@gmail.com>
2021-12-31 09:38:16 +0530

    added getcoins selector

--- src/screens/Coins.js
+++ src/screens/Coins.js
@@ -1,9 +1,13 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import { StyleSheet, Text, FlatList } from 'react-native';
+import { useSelector, shallowEqual } from 'react-redux';
 
 import Page from '../components/Page';
+import { getCryptoCoins } from '../store/selectors/assetSelector';
 
 const Coins = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const coins = useSelector((state) => getCryptoCoins(state)(searchValue));
   return <Page scroll={false}></Page>;
 };
 
--- src/store/selectors/assetSelector.js
+++ src/store/selectors/assetSelector.js
@@ -2,9 +2,17 @@ import { createSelector } from 'reselect';
 import memoizeOne from 'memoize-one';
 import _pick from 'lodash/pick';
 import _values from 'lodash/values';
+import _filter from 'lodash/filter';
+import _toLower from 'lodash/toLower';
 
 export const assetReducer = (state) => state.assets;
 
 export const getCryptoAssets = createSelector(assetReducer, (assets) =>
   memoizeOne((coins = []) => _values(_pick(assets, coins)))
+);
+
+export const getCryptoCoins = createSelector(assetReducer, (assets) =>
+  memoizeOne((name = '') =>
+    _filter(assets, (asset) => _toLower(asset.name).includes(_toLower(name)))
+  )
 );
