29cd6dc4d2e4d985f7ed22a6463b7b322845164c
justinkx <justinkizhakkeparampil@gmail.com>
2022-01-11 21:44:12 +0530

    change app.json to app.config.js

---
+++ app.config.js
@@ -0,0 +1,71 @@
+module.exports = () => {
+  if (process.env.MY_ENVIRONMENT === 'production') {
+    return {
+      name: 'Crypto Cap',
+      description: 'Crypto app to trade with any exchange',
+      slug: 'crypto-cap',
+      version: '1.0.0',
+      orientation: 'portrait',
+      icon: './assets/icon.png',
+      privacy: 'unlisted',
+      backgroundColor: '#4f34b1',
+      splash: {
+        image: './assets/splash.png',
+        resizeMode: 'contain',
+        backgroundColor: '#4f34b1',
+      },
+      updates: {
+        fallbackToCacheTimeout: 0,
+      },
+      assetBundlePatterns: ['**/*'],
+      ios: {
+        supportsTablet: true,
+        bundleIdentifier: 'com.justinkx.cryptocap',
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: './assets/adaptive-icon.png',
+          backgroundColor: '#4f34b1',
+        },
+        package: 'com.justinkx.cryptocap',
+      },
+      web: {
+        favicon: './assets/favicon.png',
+      },
+    };
+  } else {
+    return {
+      name: 'Crypto Cap Dev',
+      description: 'Crypto app to trade with any exchange',
+      slug: 'crypto-cap-dev',
+      version: '1.0.0',
+      orientation: 'portrait',
+      icon: './assets/icon.png',
+      privacy: 'unlisted',
+      backgroundColor: '#4f34b1',
+      splash: {
+        image: './assets/splash.png',
+        resizeMode: 'contain',
+        backgroundColor: '#4f34b1',
+      },
+      updates: {
+        fallbackToCacheTimeout: 0,
+      },
+      assetBundlePatterns: ['**/*'],
+      ios: {
+        supportsTablet: true,
+        bundleIdentifier: 'com.justinkx.cryptocap-dev',
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: './assets/adaptive-icon.png',
+          backgroundColor: '#4f34b1',
+        },
+        package: 'com.justinkx.cryptocap-dev',
+      },
+      web: {
+        favicon: './assets/favicon.png',
+      },
+    };
+  }
+};
