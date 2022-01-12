0eddd2684d3f5ee69b5a6190839fd226b8c397ee
justinkx <justinkizhakkeparampil@gmail.com>
2022-01-12 18:00:01 +0530

    added checkbox component

--- src/screens/Wallets.js
+++ src/screens/Wallets.js
@@ -1,7 +1,8 @@
-import React, { memo } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { memo, useState, useCallback } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { useSelector, shallowEqual } from 'react-redux';
+import Checkbox from 'expo-checkbox';
 
 import Page from '../components/Page';
 import { commonStyles, colors } from '../styles/CommonStyles';
@@ -9,9 +10,16 @@ import { balance, allBalanceCoins } from '../utils/data';
 import { pickCryptoAssets } from '../store/selectors/assetSelector';
 
 const Wallets = () => {
+  const [isMarketTicker, setMarketTicker] = useState(true);
+
   const balanceTickers = useSelector(
     (state) => pickCryptoAssets(state)(allBalanceCoins),
     shallowEqual
+  );
+
+  const handleCheckbox = useCallback(
+    () => setMarketTicker((prev) => !prev),
+    []
   );
 
   return (
@@ -21,6 +29,19 @@ const Wallets = () => {
           <Text>Total</Text>
           <FontAwesome name="dollar" size={24} color={colors.white} />
         </View>
+        <View style={styles.checkBoxView}>
+          <TouchableOpacity style={commonStyles.row} onPress={handleCheckbox}>
+            <Checkbox
+              style={styles.checkbox}
+              value={isMarketTicker}
+              onValueChange={setMarketTicker}
+              color={isMarketTicker ? colors.success : colors.white}
+            />
+            <Text style={[styles.marketTicker, commonStyles.fontBold]}>
+              Market
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </Page>
   );
@@ -29,6 +50,18 @@ const Wallets = () => {
 export default memo(Wallets);
 
 const styles = StyleSheet.create({
-  balanceView: { marginVertical: 25 },
+  balanceView: { marginVertical: 25, position: 'relative', height: 150 },
   total: {},
+  checkBoxView: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+  checkbox: {
+    marginRight: 5,
+  },
+  marketTicker: {
+    fontSize: 14,
+    color: colors.white,
+  },
 });
