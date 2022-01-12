86f661e91791e363bb86e994afb402b729819e63
justinkx <justinkizhakkeparampil@gmail.com>
2022-01-12 16:52:35 +0530

    added CRYPTO_ASSETS_SPARKLINE api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,10 @@ import _toLower from 'lodash/toLower';
 
 export const CRYPTO_ASSETS =
   'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false&price_change_percentage=24h';
+
+export const CRYPTO_ASSETS_SPARKLINE =
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=true&price_change_percentage=24h';
+
 export const CRYPTO_EXCHANGES = 'https://api.coingecko.com/api/v3/exchanges';
 export const CRYPTO_MARKETS = 'https://api.coincap.io/v2/markets';
 
